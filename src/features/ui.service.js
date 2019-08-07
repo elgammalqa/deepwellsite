94b944145aa54e6dcbf7fcc72aa03be089e422ee
Amir ElGammal <elgammalqa@gmail.com>
2019-08-07 14:11:53 +0200

    services ui

--- src/features/ui.service.js
+++ src/features/ui.service.js
@@ -1,8 +1,7 @@
 import {action} from 'mobx'
 import _ from 'lodash'
 
-const STATUS_PAGE_URL = 'https://6l1kzwgr7t06.statuspage.io/api/v2/status.json'
-const STATUS_PAGE_TIMEOUT = 5000
+
 
 export default class UI {
   @action.bound toggleFlag (flag, value) {
@@ -17,10 +16,7 @@ export default class UI {
     this.store.modalData = data
     this.store.modal = this.store.modal === name ? null : name
 
-    window.analytics.page(`Website - ${_.upperFirst(name)}`, {
-      path: `#/${name}`,
-      title: `Website - ${_.upperFirst(name)}`
-    })
+
   }
 
   @action.bound async fetchServicesStatus () {
